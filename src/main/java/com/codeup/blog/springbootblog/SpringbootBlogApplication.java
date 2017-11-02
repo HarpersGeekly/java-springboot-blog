68e140e81bd0d9fde5c80dcb6da6c01644a3f2d2
HarpersGeekly <HarperRyanC@gmail.com>
2017-11-02 09:28:57 -0500

    Initial commit

---
+++ src/main/java/com/codeup/blog/springbootblog/SpringbootBlogApplication.java
@@ -0,0 +1,12 @@
+package com.codeup.blog.springbootblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogApplication.class, args);
+	}
+}
