a81a5a747299d8b94c19324e3ab750f91651102e
HarpersGeekly <HarperRyanC@gmail.com>
2017-11-10 10:06:37 -0600

    -Prepared files .build_config and SpringbootBlogApplication for deployment if needed. -Font-size changes.

--- src/main/java/com/codeup/blog/springbootblog/SpringbootBlogApplication.java
+++ src/main/java/com/codeup/blog/springbootblog/SpringbootBlogApplication.java
@@ -2,11 +2,17 @@ package com.codeup.blog.springbootblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootBlogApplication {
+public class SpringbootBlogApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBlogApplication.class, args);
+	}
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootBlogApplication.class);
 	}
 }
