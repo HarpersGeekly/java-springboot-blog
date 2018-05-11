c16a9beb4eb761a1aca6269f2d7b562fe7a7d1b3
HarpersGeekly <HarperRyanC@gmail.com>
2018-05-11 12:37:59 -0500

    FileUploadController has a String property for the fileUpload path. The form sends the MulitpartFile as a param. I grab the User from the session, then grab that User in the database. Then get the String filepath = Paths.get(uploadedPath, loggedInUser.profilePicturePath()).toString); Create new File(filepath), update the profilePicture on the loggedInUser, and then save that user. After a user updates their picture, reset the current principal loggedinUser with the changes applied so that the profile picture won't show the default placeholder after they reload the page.

--- src/main/java/com/codeup/blog/springbootblog/controllers/FileUploadController.java
+++ src/main/java/com/codeup/blog/springbootblog/controllers/FileUploadController.java
@@ -3,21 +3,17 @@ package com.codeup.blog.springbootblog.controllers;
 import com.codeup.blog.springbootblog.Models.User;
 import com.codeup.blog.springbootblog.repositories.UsersRepository;
 import com.codeup.blog.springbootblog.services.UserService;
+import com.codeup.blog.springbootblog.services.UserWithRoles;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Paths;
+import java.util.Collections;
 
 @Controller
 public class FileUploadController {
@@ -40,63 +36,28 @@ public class FileUploadController {
 
         @PostMapping("/profile/edit/fileupload")
         public @ResponseBody User saveFile(
-                @RequestParam(name = "croppedImage") MultipartFile uploadedFile,
-                Model model
-        ) {
+                @RequestParam(name = "croppedImage") MultipartFile uploadedFile) {
+            //let exception be thrown for maxsize and solve the issue in the ajax.done with if fails, then show html in the partial
+
+            System.out.println("get to FileUploadController");
+
             User loggedInUser = userSvc.loggedInUser(); // is a user from the session
             loggedInUser = usersDao.findById(loggedInUser.getId()); //now is a user from the database
-
-//            String filename = uploadedFile.getOriginalFilename(); ////comment this out
-            String filepath = Paths.get(uploadPath, loggedInUser.getProfilePicture()).toString();
-//            String filepath = Paths.get(uploadPath, filename).toString();////comment this out
-
+            String filepath = Paths.get(uploadPath, loggedInUser.profilePicturePath()).toString(); //first is path, second is additional string to be joined to form the final path string
             File destinationFile = new File(filepath);
+            loggedInUser.updateProfilePicture();
+            usersDao.save(loggedInUser);
 
-//            User user = usersDao.findOne(loggedInUser.getId());
-//            loggedInUser.setProfilePicture(filepath); //we deleted this.////comment this out
-//            usersDao.save(loggedInUser);//we deleted this.////comment this out
+            //Reset the current principal loggedinUser with the changes applied so that the profile picture won't show the default placeholder after they reload the page.
+            UserWithRoles principal = new UserWithRoles(loggedInUser, Collections.emptyList());
+            Authentication authentication = new UsernamePasswordAuthenticationToken(principal, principal.getPassword(), principal.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
             try {
                 uploadedFile.transferTo(destinationFile);
-                model.addAttribute("message", "File successfully uploaded!");
             } catch (IOException e) {
                 e.printStackTrace();
-                model.addAttribute("message", "Oops! Something went wrong! " + e);
             }
             return loggedInUser;
         }
     }
-
-//    @GetMapping("img-display")
-//    public String showUploadFileForm() {
-//        return "img-display";
-//    }
-
-//    @PostMapping("/profile/image")
-//    @ResponseBody
-//    public String saveFile(
-//            //@RequestParam(name = "img-display") MultipartFile uploadedFile,
-//            @RequestParam(value = "url", required = false) String url,
-//            @RequestParam(value = "id", required = false) Long userId) {
-//        System.out.println("[saveFile] url: " + url);
-//
-//        User loggedInUser = userSvc.loggedInUser();
-//        User user = usersDao.findOne(loggedInUser.getId());
-//        user.setProfilePicture(url);
-//        usersDao.save(user);
-//        // INJECT THE USER REPOSITORY
-//        // CALL THE METHOD findOne USING THE userId variable
-//        // images.setProfilePic(url);
-//        // usersRepository.save(user);
-//
-////            String filename = uploadedFile.getOriginalFilename();
-////            String filepath = Paths.get(uploadPath, filename).toString();
-////            File destinationFile = new File(filepath);
-////            try {
-////                uploadedFile.transferTo(destinationFile);
-////                model.addAttribute("message", "File successfully uploaded!");
-////            } catch (IOException e) {
-////                e.printStackTrace();
-////                model.addAttribute("message", "Oops! Something went wrong! " + e);
-////            }
-//        return "redirect:/profile";
-//    }
