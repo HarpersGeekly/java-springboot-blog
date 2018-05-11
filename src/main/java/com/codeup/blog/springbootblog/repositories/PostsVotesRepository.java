c762443981d41e6ef8f0898131267d75af75d9b6
HarpersGeekly <HarperRyanC@gmail.com>
2018-05-11 15:42:18 -0500

    Karma query totals the number of votes attached to a users Post.

--- src/main/java/com/codeup/blog/springbootblog/repositories/PostsVotesRepository.java
+++ src/main/java/com/codeup/blog/springbootblog/repositories/PostsVotesRepository.java
@@ -9,9 +9,15 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PostsVotesRepository extends CrudRepository<PostVote, PostVoteId>{
 
+    //    There is no Boolean type in Oracle SQL. You will need to return a 1 or 0, or some such and act accordingly:
     @Query(nativeQuery = true,
             value="SELECT CASE WHEN COUNT(post_id) > 0 THEN 1 ELSE 0 END FROM posts_votes WHERE user_id =?1")
     int hasVoted(Long id);
 
-//    There is no Boolean type in Oracle SQL. You will need to return a 1 or 0, or some such and act accordingly:
+    @Query(nativeQuery = true,
+            value = "SELECT SUM(type) FROM posts_votes pv " +
+                    "LEFT JOIN posts p ON p.id = pv.post_id " +
+                    "inner JOIN users u ON u.id = p.user_id " +
+                    "WHERE u.id = ?1;")
+    long totalPostKarmaForUser(Long id);
 }
