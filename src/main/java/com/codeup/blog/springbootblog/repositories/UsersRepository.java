5fcb72a3d1fe541347d6d9b8841a875784764c47
HarpersGeekly <HarperRyanC@gmail.com>
2018-05-21 10:01:28 -0500

    totalKarma queries and sums up the total vote type a user has received on both posts and comments. It selects the sums from two separate repositories and then adds them together. SELECT IFNULL(, 0) will allow for a nullable returns to instead reveal a "0". This allows the page to not return nullpointerexceptions if a user doesn't have any karma.

--- src/main/java/com/codeup/blog/springbootblog/repositories/UsersRepository.java
+++ src/main/java/com/codeup/blog/springbootblog/repositories/UsersRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Created by RyanHarper on 11/7/17.
  */
@@ -19,6 +21,15 @@ public interface UsersRepository extends CrudRepository<User, Long> {
 
     // select * from users where email = ?
     User findByEmail(String password);
+
+    @Query(nativeQuery = true,
+            value = "SELECT " +
+                    "(SELECT IFNULL(SUM(type), 0) FROM posts_votes pv LEFT JOIN posts p ON p.id = pv.post_id inner JOIN users u ON u.id = p.user_id  WHERE u.id LIKE ?1)" +
+                    " + (SELECT IFNULL(SUM(type), 0) FROM comments_votes cv LEFT JOIN comments c ON c.id = cv.comment_id inner JOIN users u ON u.id = c.user_id WHERE u.id LIKE ?1)")
+    long totalKarma(Long id);
+
+//    @Query()
+//    List<User> popularUsersByKarma;
 
 
 //    @Query("select * from users u where u.email = ?1")
