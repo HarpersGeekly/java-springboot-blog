102b764b4120131e5f6080b7e29224569522948b
HarpersGeekly <HarperRyanC@gmail.com>
2018-06-04 15:56:56 -0500

    search posts will return the List in descending order of id, which is essentially the same as create date.

--- src/main/java/com/codeup/blog/springbootblog/repositories/PostsRepository.java
+++ src/main/java/com/codeup/blog/springbootblog/repositories/PostsRepository.java
@@ -17,7 +17,7 @@ import java.util.List;
 public interface PostsRepository extends CrudRepository<Post, Long> { // <Model, Primary Key>
     // There are built in methods for CRUDRepository, findAll(), findOne(), save(), delete().
     // Is not SQL query...it's HQL (hibernate) talking to the database not by table relationships but by object relationships
-    @Query("SELECT p FROM Post p JOIN p.user u JOIN p.categories c WHERE p.title LIKE ?1 OR p.description LIKE ?1 OR u.username LIKE ?1 OR c.name LIKE ?1")
+    @Query("SELECT DISTINCT p FROM Post p JOIN p.user u JOIN p.categories c WHERE p.title LIKE ?1 OR p.description LIKE ?1 OR u.username LIKE ?1 OR c.name LIKE ?1 ORDER BY p.id DESC")
     List<Post> searchPostsWithKeyword(String term);
 
     @Query(nativeQuery = true,
