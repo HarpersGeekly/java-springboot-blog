a29e9de2f8be8568fd5cf55dc9199ac93f2f1f86
HarpersGeekly <HarperRyanC@gmail.com>
2018-06-22 15:21:50 -0500

    -had to take into account hidden when there ISN'T an image.
    $('.postTextBodyDiv').each(function() {
        if ($(this).find('img').length) {
           // there is an image in this div, do something...
        }
        $('.postTextBodyDiv').removeClass('hidden'); // otherwise
    }

--- src/main/resources/static/js/myImageResizer.js
+++ src/main/resources/static/js/myImageResizer.js
@@ -1,35 +1,39 @@
-$('.postTextBodyDiv img').each(function() {
+$('.postTextBodyDiv').each(function() {
+    if ($(this).find('img').length) {
+        // there is an image in this div, do something...
+        let maxWidth = 700;
+        let ratio = 0;
+        let img = $(this);
 
-    let maxWidth = 700;
-    let ratio = 0;
-    let img = $(this);
-
-    if(img.width() > maxWidth) {
-        ratio = img.height() / img.width();
-        img.css("height", (maxWidth * ratio));
-        img.css("width", maxWidth);
-        // To prevent the DOM bouncing around during resizing, hide it first.
-        // Add a "hidden" (display:none in css) class to the html element $() then remove it after resizing is complete
-        $('.postTextBodyDiv').removeClass('hidden');
-        // img.attr("height", (maxWidth * ratio));
-        // img.attr("width", maxWidth);
+        if (img.width() > maxWidth) {
+            ratio = img.height() / img.width();
+            img.css("height", (maxWidth * ratio));
+            img.css("width", maxWidth);
+            // To prevent the DOM bouncing around during resizing, hide it first.
+            // Add a "hidden" (display:none in css) class to the html element $() then remove it after resizing is complete
+            $('.postTextBodyDiv').removeClass('hidden');
+            // img.attr("height", (maxWidth * ratio));
+            // img.attr("width", maxWidth);
+        }
     }
-    $('.postTextBodyDiv').removeClass('hidden');
+        $('.postTextBodyDiv').removeClass('hidden');
 });
 
-$('.postTextBodyDiv img').load(function() {
+$('.postTextBodyDiv').load(function() {
+    if ($(this).find('img').length) {
+        // there is an image in this div, do something...
+        let maxWidth = 700;
+        let ratio = 0;
+        let img = $(this);
 
-    let maxWidth = 700;
-    let ratio = 0;
-    let img = $(this);
-
-    if(img.width() > maxWidth) {
-        ratio = img.height() / img.width();
-        img.css("height", (maxWidth * ratio));
-        img.css("width", maxWidth);
-        $('.postTextBodyDiv').removeClass('hidden');
-        // img.attr("height", (maxWidth * ratio));
-        // img.attr("width", maxWidth);
+        if (img.width() > maxWidth) {
+            ratio = img.height() / img.width();
+            img.css("height", (maxWidth * ratio));
+            img.css("width", maxWidth);
+            $('.postTextBodyDiv').removeClass('hidden');
+            // img.attr("height", (maxWidth * ratio));
+            // img.attr("width", maxWidth);
+        }
     }
-    $('.postTextBodyDiv').removeClass('hidden');
+        $('.postTextBodyDiv').removeClass('hidden');
 });
