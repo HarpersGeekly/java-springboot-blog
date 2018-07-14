a0204696463740c5731c1ed2a4f8678c595db30e
HarpersGeekly <HarperRyanC@gmail.com>
2018-07-14 17:47:27 -0500

    Now has a method that takes in a String. This String will be split into an array and for the length of this array, each first letter in each element will be capitalized

--- src/main/java/com/codeup/blog/springbootblog/Models/Post.java
+++ src/main/java/com/codeup/blog/springbootblog/Models/Post.java
@@ -227,4 +227,15 @@ public class Post {
         HtmlRenderer renderer = HtmlRenderer.builder().build();
         return renderer.render(parser.parse(title));
     }
+
+    public String toTitleCase(String title) {
+        String[] arr = title.split(" ");
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(Character.toUpperCase(arr[i].charAt(0)))
+                    .append(arr[i].substring(1)).append(" ");
+        }
+        return sb.toString().trim();
+    }
 }
