d1a9b2a276d3b50e2ba3628b0e96582c96002b6c
HarpersGeekly <HarperRyanC@gmail.com>
2018-08-23 14:53:17 -0500

    Delete methods available for use. Deletes reset tokens based on user_id

--- src/main/java/com/codeup/blog/springbootblog/repositories/PasswordRepository.java
+++ src/main/java/com/codeup/blog/springbootblog/repositories/PasswordRepository.java
@@ -1,10 +1,13 @@
 package com.codeup.blog.springbootblog.repositories;
 
 import com.codeup.blog.springbootblog.Models.PasswordToken;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 public interface PasswordRepository extends CrudRepository<PasswordToken, Long> {
@@ -13,4 +16,9 @@ public interface PasswordRepository extends CrudRepository<PasswordToken, Long> 
 
     @Transactional
     void deleteById(Long id);
+
+    @Modifying
+    @Transactional
+    @Query(nativeQuery = true, value="DELETE FROM reset_password WHERE user_id LIKE ?1")
+    void deleteByUserId(Long id);
 }
