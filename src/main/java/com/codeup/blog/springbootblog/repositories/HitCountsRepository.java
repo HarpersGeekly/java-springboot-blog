122fcf54b1902201e63f0915aa0962aaa45e777d
HarpersGeekly <HarperRyanC@gmail.com>
2018-08-28 14:04:57 -0500

    repository for HitCount. Unused method, not needed at the moment.

---
+++ src/main/java/com/codeup/blog/springbootblog/repositories/HitCountsRepository.java
@@ -0,0 +1,13 @@
+package com.codeup.blog.springbootblog.repositories;
+
+import com.codeup.blog.springbootblog.Models.HitCount;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HitCountsRepository extends CrudRepository<HitCount, Long> {
+
+    @Query(nativeQuery = true, value="SELECT count FROM hit_counts hc WHERE hc.post_id LIKE ?1")
+    int hitCountByPost(Long id);
+}
