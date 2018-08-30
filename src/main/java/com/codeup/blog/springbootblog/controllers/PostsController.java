b31dceacadca4519b120f1eac30095900d1579cb
HarpersGeekly <HarperRyanC@gmail.com>
2018-08-30 11:58:11 -0500

    search page has the results from mostViewedPosts

--- src/main/java/com/codeup/blog/springbootblog/controllers/PostsController.java
+++ src/main/java/com/codeup/blog/springbootblog/controllers/PostsController.java
@@ -292,6 +292,7 @@ public class PostsController {
         viewModel.addAttribute("categories", categoriesDao.findAll());
         viewModel.addAttribute("mostCommentedPosts", postSvc.popularPostsByCommentActivity());
         viewModel.addAttribute("mostLikedPosts", postSvc.popularPostsByLikes());
+        viewModel.addAttribute("mostViewedPosts", postSvc.popularPostsByViews());
         viewModel.addAttribute("popularUsers", usersDao.popularUsersByKarma());
         viewModel.addAttribute("karmas", usersDao.popularUsersKarma());
         return "/posts/search";
