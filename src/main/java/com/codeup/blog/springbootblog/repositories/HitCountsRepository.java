da9b63da641f3e7c6790e93a97d03ab79ee83a69
HarpersGeekly <HarperRyanC@gmail.com>
2018-08-30 11:15:46 -0500

    added method, unused, to ever return a count by calling the dao, rather than calling on properties

--- src/main/java/com/codeup/blog/springbootblog/repositories/HitCountsRepository.java
+++ src/main/java/com/codeup/blog/springbootblog/repositories/HitCountsRepository.java
@@ -10,4 +10,7 @@ public interface HitCountsRepository extends CrudRepository<HitCount, Long> {
 
     @Query(nativeQuery = true, value="SELECT count FROM hit_counts hc WHERE hc.post_id LIKE ?1")
     int hitCountByPost(Long id);
+
+    @Query(nativeQuery = true, value="SELECT count FROM hit_counts hc WHERE hc.user_id LIKE ?1")
+    int hitCountByUser(Long id);
 }
