a784b9a20fa4e8adbb8e12392f45903ed4c09ecb
HarpersGeekly <HarperRyanC@gmail.com>
2018-09-04 15:28:55 -0500

    Made a RolesRepository that uses a Hibernate query to find a User from the username and then the user_id

---
+++ src/main/java/com/codeup/blog/springbootblog/repositories/RolesRepository.java
@@ -0,0 +1,15 @@
+package com.codeup.blog.springbootblog.repositories;
+
+import com.codeup.blog.springbootblog.Models.UserRole;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RolesRepository extends CrudRepository<UserRole, Long> {
+
+        @Query("select ur.role from UserRole ur, User u where u.username=?1 and ur.userId = u.id")
+        List<String> ofUserWith(String username);
+}
