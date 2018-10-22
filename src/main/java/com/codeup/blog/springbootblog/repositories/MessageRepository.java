d70a2fd976ef92b41bc422f9680b914a70ca1349
HarpersGeekly <HarperRyanC@gmail.com>
2018-10-22 17:09:41 -0500

    repository for Message in database

--- src/main/java/com/codeup/blog/springbootblog/repositories/MessageRepository.java
+++ src/main/java/com/codeup/blog/springbootblog/repositories/MessageRepository.java
@@ -4,6 +4,10 @@ import com.codeup.blog.springbootblog.Models.Message;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MessageRepository extends CrudRepository<Message, Long> {
+
+    List<Message> findAllByReceiverId(Long id);
 }
